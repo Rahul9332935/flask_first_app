96866a27675ca8bcf697b11a90e7eb1377177e0b
Rahul9332935 <88732123+Rahul9332935@users.noreply.github.com>
2023-10-16 05:48:59 +0000

    done

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from flask import Flask, jsonify, make_response, request
 import configDb
+from flask_cors import CORS
 
 app = Flask(__name__)
-
+CORS(app)
 @app.route("/")
 def hello():
   return "hello"
